461592f3522d5e4c381aecce6c06e8ad774ae489
alihussain575 <alihussainirfan575@gmail.com>
2022-03-29 22:46:51 +0500

    added upload image

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,19 +4,19 @@ const User = require('../models/user');
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, 'thisismynewcourse');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
       'tokens.token': token,
     });
 
-    if(!user) {
-      throw new Error()
+    if (!user) {
+      throw new Error();
     }
 
     req.token = token;
-    req.user = user
-    next()
+    req.user = user;
+    next();
   } catch (e) {
     res.status(401).send({ error: 'please authenticate' });
   }
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const multer = require('multer');
+const nodemailer = require('nodemailer');
+const sharp = require('sharp');
 const User = require('../models/user');
-const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
+const upload = require('../middleware/upload');
 const { successLog, errorLog } = require('../colors/colors');
 
 const router = new express.Router();
@@ -13,6 +16,30 @@ router.post('/users', async (req, res) => {
     await users.save();
     successLog(users);
     const token = await users.generateAuthToken();
+
+    // let transporter = nodemailer.createTransport({
+    //   host: 'smtp.ethereal.email',
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: 'alihussainirfan575@gmail.com',
+    //     pass: 'alihussainkhan',
+    //   },
+    // });
+
+    // let mailOptions = {
+    //   from: 'alihussainirfan575@gmail.com',
+    //   to: `${req.users.email}`,
+    //   subject: 'description',
+    //   text: `details:`,
+    //   html: `<>
+    //   <h1>Welcome to Task Manager</h1>
+    //   <p>Hello ${req.users.name} </br>
+    //     Your Account was created
+    //   </p>
+    //   </>`,
+    // };
+
     res.status(201).send({
       users,
       token,
@@ -72,6 +99,29 @@ router.post('/users/login', async (req, res) => {
       req.body.password
     );
     const token = await user.generateAuthToken();
+    // let transporter = nodemailer.createTransport({
+    //   host: 'smtp.ethereal.email',
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: 'alihussainirfan575@gmail.com',
+    //     pass: 'alihussainkhan',
+    //   },
+    // });
+
+    // let mailOptions = {
+    //   from: 'alihussainirfan575@gmail.com',
+    //   to: `${req.body.email}`,
+    //   subject: 'description',
+    //   text: `details:`,
+    //   html: `<>
+    //   <h1>Welcome to Task Manager</h1>
+    //   <p>Hello ${req.body.name} </br>
+    //     You have logged in successfully
+    //   </p>
+    //   </>`,
+    // };
+
     successLog(user);
     res.status(200).send({
       user,
@@ -91,6 +141,29 @@ router.post('/users/logout', auth, async (req, res) => {
 
     await req.user.save();
 
+    // let transporter = nodemailer.createTransport({
+    //   host: 'smtp.ethereal.email',
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: 'alihussainirfan575@gmail.com',
+    //     pass: 'alihussainkhan',
+    //   },
+    // });
+
+    // let mailOptions = {
+    //   from: 'alihussainirfan575@gmail.com',
+    //   to: `${req.user.email}`,
+    //   subject: 'description',
+    //   text: `details:`,
+    //   html: `<>
+    //   <h1>Welcome to Task Manager</h1>
+    //   <p>Hello ${req.user.name} </br>
+    //     you have logged out
+    //   </p>
+    //   </>`,
+    // };
+
     successLog(req.user);
     res.status(200).send(req.user);
   } catch (e) {
@@ -104,11 +177,79 @@ router.post('/users/logoutAll', auth, async (req, res) => {
     req.user.tokens = [];
     await req.user.save();
 
+    // let transporter = nodemailer.createTransport({
+    //   host: 'smtp.ethereal.email',
+    //   port: 587,
+    //   secure: false,
+    //   auth: {
+    //     user: 'alihussainirfan575@gmail.com',
+    //     pass: 'alihussainkhan',
+    //   },
+    // });
+
+    // let mailOptions = {
+    //   from: 'alihussainirfan575@gmail.com',
+    //   to: `${req.user.email}`,
+    //   subject: 'description',
+    //   text: `details:`,
+    //   html: `<>
+    //   <h1>Welcome to Task Manager</h1>
+    //   <p>Hello ${req.user.name} </br>
+    //     you have logged out of all devices
+    //   </p>
+    //   </>`,
+    // };
+
     successLog(req.user);
     res.status(200).send(req.user);
   } catch (e) {
     errorLog(e);
     res.status(500).send(e);
+  }
+});
+
+router.post(
+  '/users/user-profile/upload',
+  auth,
+  upload.single('avatar'),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 500, height: 500 })
+      .png()
+      .toBuffer();
+
+    req.user.avatar = buffer;
+
+    await req.user.save(req.user.avatar);
+
+    res.send('uploaded');
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+router.delete('/users/user-profile/upload', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+
+  res.send('Deleted');
+});
+
+router.get('/users/:id/upload', async (req, res) => {
+  try {
+    const user = await User.findById({ _id: req.params.id });
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set('Content-type', 'image/png');
+
+    res.send(user.avatar);
+  } catch (e) {
+    errorLog(e);
+    res.status(500).send();
   }
 });
 
